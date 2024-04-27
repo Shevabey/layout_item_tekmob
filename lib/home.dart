95f60a920576cf781c702310612634832be5b2ce
Shevabey <shevabey100@gmail.com>
2024-04-27 23:51:44 +0700

    update homepage

--- lib/home.dart
+++ lib/home.dart
@@ -124,32 +124,3 @@ class HomePage extends StatelessWidget {
     );
   }
 }
-
-// List<Card> _buildGridCards(int count) {
-//   List<Card> cards = List.generate(count, (int index) {
-//     return Card(
-//       clipBehavior: Clip.antiAlias,
-//       child: Column(
-//         crossAxisAlignment: CrossAxisAlignment.start,
-//         children: <Widget>[
-//           AspectRatio(
-//             aspectRatio: 18.0 / 11.0,
-//             child: Image.asset('assets/diamond.png'),
-//           ),
-//           Padding(
-//             padding: const EdgeInsets.fromLTRB(16.0, 12.0, 16.0, 8.0),
-//             child: Column(
-//               crossAxisAlignment: CrossAxisAlignment.start,
-//               children: <Widget>[
-//                 Text('Title'),
-//                 const SizedBox(height: 8.0),
-//                 Text('Secondary Text'),
-//               ],
-//             ),
-//           ),
-//         ],
-//       ),
-//     );
-//   });
-//   return cards;
-// }
